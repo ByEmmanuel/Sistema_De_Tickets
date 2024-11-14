046086268645c53a7408ea653f46de4aa8014693
Abr16jin <axeltriple1603@gmail.com>
2024-11-13 21:41:27 -0600

    Funcion actualizar, trabajando en menu secundario

--- Bang.cpp
+++ Bang.cpp
@@ -8,21 +8,37 @@
 #include <thread>
 using namespace std; 
 int userop;
+int user_acces;
+int second_acces;
 int id;
 
 //función menú
 void main_menu(){
-        cout << "\n\t\t\t---Bienvenido al Sistema de Tickets--- (2)" << endl;
-        cout << "\n\tSeleccione una operación:"<<endl;
-        cout << "\t1) Búsqueda de Usuario"<<endl;
-        cout << "\t2) Actualizar datos de Usuario"<<endl;
-        cout << "\t3) Registrar Usuario"<<endl;
-        cout << "\t4) Borrar Usuario"<<endl;
-        cout << "\t5) Salir"<<endl;
-        cout << "\n\t\t\t===================================="<<endl;
+        cout << "\n\t\t\t---Bienvenido al Sistema de Tickets---" << endl;
+        cout << "\n\t╦ Seleccione una operación:═══════════╗"<<endl;
+        cout << "\t╠ 1) Búsqueda de Usuario              ║"<<endl;
+        cout << "\t╠ 2) Actualizar datos de Usuario      ║"<<endl;
+        cout << "\t╠ 3) Registrar Usuario                ║"<<endl;
+        cout << "\t╠ 4) Borrar Usuario                   ║"<<endl;
+        cout << "\t╠ 5) Acción de Usuario                ║"<<endl;
+        cout << "\t╠ 6) Salir                            ║" <<endl;
+        cout << "\t╚═════════════════════════════════════╝"<<endl;
         cout << "\nEjecutar operación ─> ";
         }
-
+//Funcion menú secundario//
+//función menú
+void second_menu(){
+        cout << "\n\t\t\t---Acciones de Usuario---" << endl;
+      cout << "\n\t╦ Seleccione una operación:═════════════════════╗"<<endl;
+        cout << "\t╠ 1) Generar cita                               ║"<<endl;
+        cout << "\t╠ 2) Consultar cita                             ║"<<endl;
+        cout << "\t╠ 3) Cancelar cita                              ║"<<endl;
+        cout << "\t╠ 4) Consulta de citas disponibles              ║"<<endl;
+        cout << "\t╠ 5) Horarios de Atención                       ║"<<endl;
+        cout << "\t╠ 6) Volver a inicio                            ║" <<endl;
+        cout << "\t╚═══════════════════════════════════════════════╝"<<endl;
+        cout << "\nEjecutar operación ─> ";
+        }
 //  v v v  // User vector //
 struct user{
     int id;
@@ -32,8 +48,16 @@ struct user{
     int passnum;
     string referencia;
     long long phone_number;
-    string user_email;
-    
+    string user_email;    
+    };
+    //  v v v  // Cita vector //
+struct cita{
+    long long id_cita;
+    string operacion_cita;
+    string area;
+    string departamento;    
+    string referencia;    
+    //agregar personal que atiende    
     };
 // funcion generacion de ID random único
 int identification_Number() {
@@ -131,7 +155,58 @@ void show_user_details(const user& u) {
     cout << "Correo asociado: "<< u.user_email << endl;
     cout << "Número telefónico: " << u.phone_number << endl;
 }
+//Función 4 Actualizar
+void update_user(vector<user>& users, int id) {
+  
+    user* u = search_user_by_id(users, id);    
+    if (u != nullptr) {  
+        int option;
+        cout << "Usuario encontrado: " << u->nombre << " " << u->apellidop << " " << u->apellidom << endl;
+        cout << "¿Qué campo deseas actualizar?\n";
+        cout << "1. Nombre\n";
+        cout << "2. Apellido Paterno\n";
+        cout << "3. Apellido Materno\n";
+        cout << "4. Número de Teléfono\n";
+        cout << "5. Correo Electrónico\n";
+        cout << "6. NIP (Contraseña)\n";
+        cout << "Selecciona una opción: "; cin >> option;      
+        // Dependiendo de la opción, actualizar el campo correspondiente
+        switch (option) {
+            case 1:
+            {
+                cout << "Escribe el nuevo nombre: ";
+                cin.ignore(); 
+                getline(cin, u->nombre);
+                break;}
+            case 2:{
+                cout << "Escribe el nuevo apellido paterno: ";
+                cin.ignore();  
+                getline(cin, u->apellidop);
+                break;}
+            case 3:{
+                cout << "Escribe el nuevo apellido materno: ";
+                cin.ignore();  
+                getline(cin, u->apellidom);
+                break;}
+            case 4:{
+                u->phone_number = user_phone_Number(); 
+                break;}
+            case 5:{
+                user_Mail(u->user_email); 
+                break;}
+            case 6:{
+                u->passnum = pass_Number(); 
+                break;}
+            default:
+                cout << "\033[1;31mOpción inválida. No se realizó ninguna actualización.\033[0m\n";
+                return;
+        }
 
+        cout << "\033[1;32mLos datos del usuario han sido actualizados exitosamente.\033[0m\n";
+    } else {
+        cout << "\033[1;31mUsuario con ID " << id << " no encontrado.\033[0m\n";
+    }
+}
 
 
 int main(){
@@ -151,25 +226,20 @@ int main(){
                 if (encontrado != nullptr) {
                     cout << "\n\033[1;32mUsuario encontrado.\033[0m\n"<<endl;
                     show_user_details(*encontrado);
+                    this_thread::sleep_for(chrono::seconds(3));
                                         
                 } else {
                     cout << "\033[1;31mUsuario no encontrado.\033[0m\n"; 
-                    this_thread::sleep_for(chrono::seconds(2));
-                    cout<<"\033[1;31;47mPresiona ENTER para continuar...\033[0m";
-                    cin.get();                   
+                    this_thread::sleep_for(chrono::seconds(2));                                      
                 }                      
           break; 
         }
-        case 2:{
-            cout<<"\n\t\t\t===> Actualizar Datos <==="<<endl;
-            cout<<"\nIngrese su número de ticket "<<endl;
-            cout<<"\n--> Lo puedes encontrar en la esquina inferior derecha del formato"<<endl;
-            cout<<"O ingresa (x) para salir."<<endl;
-            cout<<"ID...";
-            cin>> id;
-        
+        case 2:{             
+            cout << "Ingresa el ID del usuario a actualizar -> ";cin >>id;
+            update_user(users, id);        
+            this_thread::sleep_for(chrono::seconds(2));
             break;
-        }
+             }
         case 3:{
               cout<<"\n\t\t\t===> Nuevo Usuario <==="<<endl;
               cout<<"\n\t\t\t-+ A continuación, por favor, ingrese los siguientes datos \npara crear un nuevo formato de -ticket-"<<endl;
@@ -185,7 +255,8 @@ int main(){
                 cout << "╠  Teléfono: " << nuevo_usuario.phone_number << endl;
                 cout << "╠  NIP: " << nuevo_usuario.passnum << endl;
                 cout << "╚══════════════════════════════════════════╝"<< endl;
-                cout << "\033[1;32mTerminado exitosamente.\033[0m\n"<<endl;                  
+                cout << "\033[1;32mTerminado exitosamente.\033[0m\n"<<endl; 
+                this_thread::sleep_for(chrono::seconds(3));                 
                 break;             
             }
         
@@ -193,16 +264,50 @@ int main(){
             cout<<"\n\t\t\t===> Eliminar Usuario <==="<<endl;
             cout<<"Por favor ingresa tu ID\na continuación -> ";cin>>id;
             delete_user_by_id(users, id); 
-                        
+            this_thread::sleep_for(chrono::seconds(1));                        
         }
         break;
+        //ACCIONES DE USUARIO//
         case 5:{
+            second_menu();
+            cin>>user_acces;
+            do{ switch (user_acces)
+            {
+            case 1:{
+                /* code */
+            }break;
+            case 2:{
+
+            }break;
+            case 3:{
+
+            }break;
+            case 4:{
+
+            }break;
+            case 5:{
+
+            }break;
+            case 6:{
+
+            }break;
+                default:
+                cout<<"\033[1;31mOpción no válida\033[0m"<<endl;
+                cout<<"Por favor intente de nuevo."<<endl;
+                break;
+            }
+
+            }while(user_acces !=6);
+            
+           }  
+        break;
+        //SALIDA//
+        case 6:{
             cout<<"Gracias por utilizar nuestro Sistema de Tickets"<<endl;
             system("PAUSE");
             cout<<"Saliendo..."<<endl;
-            
-           }  
-        break;   
+        }  break;
+
         default:
             cout<<"\033[1;31mOpción no válida\033[0m"<<endl;
             cout<<"Por favor intente de nuevo."<<endl;
@@ -210,5 +315,5 @@ int main(){
 
 return 0; 
         }
-    }while(userop !=5);
+    }while(userop !=6);
 }
